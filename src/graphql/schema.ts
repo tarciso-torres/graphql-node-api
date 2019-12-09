a43bd89024a62fc628ae06bf76aca70aae3c5080
Tarciso Torres <tarcisoftorres@gmail.com>
2019-12-09 08:46:40 -0300

    Create first mutation

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -15,19 +15,31 @@ const users: any[] = [
 
 const typeDefs = `
     type User {
-        id: ID!,
-        name: String!,
+        id: ID!
+        name: String!
         email: String!
     }
     
     type Query {
         allUsers: [User!]!
-    }`
+    }
+    
+    type Mutation {
+        createUser(name: String!, email: String!): User
+    }
+`;
 
 const resolvers = {
     Query: {
-        allUsers: () => []
+        allUsers: () => users
+    },
+    Mutation: {
+        createUser: (parent, args) => {
+            const newUser = Object.assign({id: users.length + 1}, args);
+            users.push(newUser);
+            return newUser;
+        }
     }
-}
+};
 
 export default makeExecutableSchema({typeDefs, resolvers});
