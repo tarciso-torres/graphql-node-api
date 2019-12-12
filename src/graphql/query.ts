5844b28507e0deaf0c1014574a7b326f23bbd92d
Tarciso Torres <tarcisoftorres@gmail.com>
2019-12-12 17:12:56 -0300

    Create modulos mutation and query

---
+++ src/graphql/query.ts
@@ -0,0 +1,16 @@
+import { userQueries } from './resources/user/user.schema';
+import { postQueries } from './resources/post/post.schema';
+import { commentQueries } from './resources/comment/comment.schema';
+
+const Query = `
+    type Query {
+        # Queries
+        ${ commentQueries }
+        ${ postQueries }
+        ${ userQueries }
+    }
+`;
+
+export {
+    Query
+}
