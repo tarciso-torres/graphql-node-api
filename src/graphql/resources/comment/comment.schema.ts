a7e29dca104cda85a72361dc8244f4b17d0f379e
Tarciso Torres <tarcisoftorres@gmail.com>
2019-12-12 17:14:35 -0300

    Create schema's comment

---
+++ src/graphql/resources/comment/comment.schema.ts
@@ -0,0 +1,34 @@
+const commentTypes = `
+
+    # Comment definition type
+    type Comment {
+        id: ID!
+        comment: String!
+        createAt: String!
+        updateAt: String!
+        user: User!
+        post: Post!
+    }
+
+    input CommentInput {
+        comment: String!
+        user: Int!
+        post: Int!
+    }
+`;
+
+const commentQueries = `
+    commentsByPost(post: ID!, first: Int, offset: Int): [ Comment! ]!
+`;
+
+const commentMutations = `
+    createComment(input: CommentInput!): Comment
+    updateComment(id: ID!, input: CommentInput!): Comment
+    deleteComment(id: ID!): Boolean
+`;
+
+export {
+    commentTypes,
+    commentQueries,
+    commentMutations
+}
