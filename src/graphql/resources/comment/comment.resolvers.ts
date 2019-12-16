441ade390e2a1f18a9850e6071be876e149c21ff
Tarciso Torres <tarcisoftorres@gmail.com>
2019-12-16 17:54:05 -0300

    Finalize implamentation comment resolvers

--- src/graphql/resources/comment/comment.resolvers.ts
+++ src/graphql/resources/comment/comment.resolvers.ts
@@ -2,6 +2,7 @@ import { GraphQLResolveInfo } from "graphql";
 import { Transaction } from 'sequelize';
 
 import { DBConnection } from '../../../interfaces/DBConnectionInterface';
+import { CommentInstance } from "../../../models/CommentModel";
 
 export const commentResolvers = {
 
@@ -27,6 +28,39 @@ export const commentResolvers = {
                     limit: first,
                     offset: offset
                 });
+        }
+    }, 
+
+    Mutation: {
+        createComment: (parent, input, {db}: {db: DBConnection}, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .create(input, {transaction: t});
+            });
+        },
+
+        updateComment: (parent, {id, input}, {db}: {db: DBConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment: CommentInstance) => {
+                        if(!comment) throw new Error(`Comment with id ${id} not found!`);
+                        return comment.update(input, {transaction: t});
+                    });
+            });
+        },
+
+        deleteComment: (parent, {id, input}, {db}: {db: DBConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment: CommentInstance) => {
+                        return comment.destroy({transaction: t})
+                            .then(comment => !!comment);
+                    });
+            });
         }
     }
 }
