46d803769715c9b55090f7d14a9c6a7540b6e511
Tarciso Torres <tarcisoftorres@gmail.com>
2019-12-16 14:19:31 -0300

    Implements mutations to post resolvers

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -1,6 +1,10 @@
 import { GraphQLResolveInfo } from "graphql";
+import { Transaction } from 'sequelize';
 
 import { DBConnection } from '../../../interfaces/DBConnectionInterface';
+import { from } from "apollo-link";
+import { postMutations } from "./post.schema";
+import { PostInstance } from "../../../models/PostModel";
 
 export const postResolvers = {
 
@@ -36,6 +40,41 @@ export const postResolvers = {
                     if(!post) throw new Error(`Post with id ${id} not found!`);
                     return post;
                 });
+        }
+    },
+
+    Mutation: {
+
+        createPost:(parent, {input}, {db}: {db: DBConnection}, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .create(input, {transaction: t});
+            });
+        },
+
+        updatePost: (parent, { id, input }, {db}: {db: DBConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .findById(id)
+                    .then((post: PostInstance) => {
+                        if(!post) throw new Error(`Post with id ${id} not found!`);
+                        return postMutations.update(input, {transaction: t});
+                    });
+            });
+        },
+
+        deletePost: (parent, { id }, {db}: {db: DBConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post 
+                    .findById(id)
+                    .then((post: PostInstance) => {
+                        if(!post) throw new Error(`Post with id ${id} not found!`);
+                        return post.destroy({transaction: t})
+                            .then(post => !!post);
+                    });
+            });
         }
     }
 }
